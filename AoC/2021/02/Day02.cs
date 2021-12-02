0b8e8b17ff7a6706bb1ea97c98ecb005be97c8c5
sodr <fredrik.soderlund@gmail.com>
2021-12-02 19:03:33 +0100

    Day 02 solution

--- AoC/2021/02/Day02.cs
+++ AoC/2021/02/Day02.cs
@@ -10,14 +10,73 @@ namespace Advent_of_Code.Year_2021
     {
         public override string? Part1()
         {
-            return null;
+            string[]? commands = input.Split(Environment.NewLine);
+            string result;
+            int position = 0, depth = 0;
+
+            foreach (string command in commands)
+            {
+                string instruction = command.Split(" ")[0];
+                int value = int.Parse(command.Split(" ")[1]);
+
+                switch (instruction)
+                {
+                    case "forward":
+                        position += value;
+                        break;
+
+                    case "down":
+                        depth += value;
+                        break ;
+
+                    case "up":
+                        depth -= value;
+                        break;
+                }
+            }
+
+            result = $"Position: {position}{Environment.NewLine}";
+            result += $"Depth: {depth}{Environment.NewLine}";
+            result += $"Answer: {position*depth}";
+
+            return result;
         }
 
         public override string? Part2()
         {
-            return null;
-        }
+            string[]? commands = input.Split(Environment.NewLine);
+            string result;
+            int position = 0, depth = 0, aim = 0;
 
+            foreach (var command in commands)
+            {
+                string instruction = command.Split(" ")[0];
+                int value = int.Parse(command.Split(" ")[1]);
+
+                switch (instruction)
+                {
+                    case "forward":
+                        position += value;
+                        depth += aim * value;
+                        break;
+
+                    case "down":
+                        aim += value;
+                        break;
+
+                    case "up":
+                        aim -= value;
+                        break;
+                }
+            }
+
+            result = $"Position: {position}{Environment.NewLine}";
+            result += $"Depth: {depth}{Environment.NewLine}";
+            result += $"Answer: {position * depth}";
+
+            return result;
+        }
+             
         public Day02()
         {
             AutoSetFolderPath();
