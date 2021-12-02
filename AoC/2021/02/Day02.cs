d9e62726e3d15cad22f80e638cf91c85530d348e
Fredrik Söderlund <fredrik.soderlund@gmail.com>
2021-12-02 19:10:40 +0100

    Day 02 solution
    * Day 02 solution
    
    * Added coments

--- AoC/2021/02/Day02.cs
+++ AoC/2021/02/Day02.cs
@@ -10,14 +10,85 @@ namespace Advent_of_Code.Year_2021
     {
         public override string? Part1()
         {
-            return null;
+            string[]? commands = input.Split(Environment.NewLine);
+            string result;
+            int position = 0, depth = 0;
+
+            // Check each command
+            foreach (string command in commands)
+            {
+                string instruction = command.Split(" ")[0];     // Store instruction
+                int value = int.Parse(command.Split(" ")[1]);   // Store value
+
+                // Check instruction
+                switch (instruction)
+                {
+                    // Go foward
+                    case "forward":
+                        position += value;
+                        break;
+
+                    // Go down
+                    case "down":
+                        depth += value;
+                        break ;
+
+                    // Go up
+                    case "up":
+                        depth -= value;
+                        break;
+                }
+            }
+
+            // Make result
+            result = $"Position: {position}{Environment.NewLine}";
+            result += $"Depth: {depth}{Environment.NewLine}";
+            result += $"Answer: {position*depth}";
+
+            return result;
         }
 
         public override string? Part2()
         {
-            return null;
-        }
+            string[]? commands = input.Split(Environment.NewLine);
+            string result;
+            int position = 0, depth = 0, aim = 0;
 
+            // Check each command
+            foreach (var command in commands)
+            {
+                string instruction = command.Split(" ")[0];     // Store instruction
+                int value = int.Parse(command.Split(" ")[1]);   // Store value
+
+                // Check instruction
+                switch (instruction)
+                {
+                    // Go foward
+                    case "forward":
+                        position += value;
+                        depth += aim * value;
+                        break;
+
+                    // Aim down
+                    case "down":
+                        aim += value;
+                        break;
+
+                    // Aim up
+                    case "up":
+                        aim -= value;
+                        break;
+                }
+            }
+
+            // Make result
+            result = $"Position: {position}{Environment.NewLine}";
+            result += $"Depth: {depth}{Environment.NewLine}";
+            result += $"Answer: {position * depth}";
+
+            return result;
+        }
+             
         public Day02()
         {
             AutoSetFolderPath();
